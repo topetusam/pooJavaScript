e1f84e92e68f16ef8f4a755a1267d9180efe93f8
topetusam <67940360+topetusam@users.noreply.github.com>
2024-07-08 07:42:24 -0500

    feat: :tada: Primer ejemplo POO

---
+++ js/main.js
@@ -0,0 +1,30 @@
+// // Definición de la clase Persona
+// class Persona {
+//     // Constructor para inicializar las propiedades nombre, edad y sexo
+//     constructor(nombre, edad, sexo) {
+//         this.nombre = nombre;
+//         this.edad = edad;
+//         this.sexo = sexo;
+//     }
+
+
+//     saludar() {
+//         console.log(`Hola, soy ${this.nombre}. ¡Mucho gusto!`);
+//     }
+// }
+
+
+// let persona1 = new Persona('Ana', 25, 'Femenino');
+
+
+// persona1.saludar(); 
+
+
+
+
+
+
+
+
+
+
