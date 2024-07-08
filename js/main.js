75ab1d6bdcd76088c99c5553c6dede76c34b04d8
topetusam <67940360+topetusam@users.noreply.github.com>
2024-07-08 07:44:04 -0500

    feat: :sparkles: Segundo ejemplo

--- js/main.js
+++ js/main.js
@@ -1,30 +1,33 @@
-// // Definición de la clase Persona
-// class Persona {
-//     // Constructor para inicializar las propiedades nombre, edad y sexo
-//     constructor(nombre, edad, sexo) {
-//         this.nombre = nombre;
-//         this.edad = edad;
-//         this.sexo = sexo;
-//     }
+// Definición de la clase Persona
+class Persona {
+    constructor(nombre, edad, sexo) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.sexo = sexo;
+    }
+
+    saludar() {
+        console.log(`Hola, soy ${this.nombre}. ¡Mucho gusto!`);
+    }
+}
 
 
-//     saludar() {
-//         console.log(`Hola, soy ${this.nombre}. ¡Mucho gusto!`);
-//     }
-// }
+class Estudiante extends Persona {
+    constructor(nombre, edad, sexo, carrera) {
+        super(nombre, edad, sexo);
+        this.carrera = carrera;
+    }
+
+    estudiar() {
+        console.log(`Estoy estudiando ${this.carrera}`);
+    }
+}
 
 
-// let persona1 = new Persona('Ana', 25, 'Femenino');
+let estudiante1 = new Estudiante('Carlos', 22, 'Masculino', 'Ingeniería Informática');
 
 
-// persona1.saludar(); 
+estudiante1.saludar(); 
 
 
-
-
-
-
-
-
-
-
+estudiante1.estudiar(); 
